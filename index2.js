e5ca9e30f37655aeba625801754f87d27cdbd181
HuGGox <franquito-kiroga@hotmail.com>
2022-02-16 21:14:58 -0300

    cambios

--- index2.js
+++ index2.js
@@ -612,91 +612,118 @@
 
 // -----------------------------------------------------------------
 
-const posts = [
-    {
-        "userId": 1,
-        "id": 1,
-        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-        "userId": 1,
-        "id": 2,
-        "title": "qui est esse",
-        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-        "userId": 1,
-        "id": 3,
-        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    },
-];
+// const posts = [
+//     {
+//         "userId": 1,
+//         "id": 1,
+//         "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//     },
+//     {
+//         "userId": 1,
+//         "id": 2,
+//         "title": "qui est esse",
+//         "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+//     },
+//     {
+//         "userId": 1,
+//         "id": 3,
+//         "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+//         "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+//     },
+// ];
 
-const findPostById = (id) => {
+
+// const findPostById = (id) => {
     
-    return new Promise((resolve, reject) => {
+//     return new Promise((resolve, reject) => {
         
-        setTimeout(() => {
-            const post = posts.find(item => item.id === id)
+//         setTimeout(() => {
+//             const post = posts.find(item => item.id === id)
             
-            if(post){
-                resolve(post)
-            }else {
-                reject("no se encontro el id " + id)
-            }
+//             if(post){
+//                 resolve(post)
+//             }else {
+//                 reject("no se encontro el id " + id)
+//             }
 
-        }, 2000);
+//         }, 2000);
         
 
-    });
-}
+//     });
+// }
 
-// findPostById(1)
-// .then((post) => {
-//     console.log(post)
-//     return findPostById(2)
-// })
-//     .then((post) => {
-//         console.log(post)
-//         return findPostById(3)
-//     })
-//     .then((post) => {
-//         console.log(post)
-//         return findPostById(4)
-//     })
-// .catch(e => console.log(e));
+// // findPostById(1)
+// // .then((post) => {
+// //     console.log(post)
+// //     return findPostById(2)
+// // })
+// //     .then((post) => {
+// //         console.log(post)
+// //         return findPostById(3)
+// //     })
+// //     .then((post) => {
+// //         console.log(post)
+// //         return findPostById(4)
+// //     })
+// // .catch(e => console.log(e));
 
-// findPostById(1)
-// .then((post) => {
-//     console.log(post)
-// })
-// .catch(e => console.log(e));
+// // findPostById(1)
+// // .then((post) => {
+// //     console.log(post)
+// // })
+// // .catch(e => console.log(e));
 
-const buscar = async () => {
-    try {        
+// const buscar = async () => {
+//     try {        
 
-        const resPosts = await Promise.all([
-            findPostById(1),
-            findPostById(2)
-        ])
+//         const resPosts = await Promise.all([
+//             findPostById(1),
+//             findPostById(2)
+//         ])
 
-            console.log(resPosts[0].title, resPosts[1].title);
+//             console.log(resPosts[0].title, resPosts[1].title);
 
 
-            // const postUno = await findPostById(1)
-            // const postDos = await findPostById(2)
-            // console.log(postUno.title, postDos.title);
+//             // const postUno = await findPostById(1)
+//             // const postDos = await findPostById(2)
+//             // console.log(postUno.title, postDos.title);
 
-    } catch (error){
+//     } catch (error){
+//         console.log(error);
+//     } finally {
+//         console.log("se ejecuta si o si");
+//     }
+
+
+// }
+
+// buscar();
+
+// console.log("fin del codigo");
+
+// ------------------------------------------------------------------
+
+
+
+const url = "https://jsonplaceholder.typicode.com/posts/"
+
+// fetch(url)
+//     .then((res) => res.json())
+//     .then(data => console.log(data))
+//     .catch(e => console.log(e))
+//     .finally(() => console.log("finalizo"));
+
+const findPostById = async (id) => {
+    try {
+
+        const res = await fetch(url + id)
+        const post = await res.json()
+        console.log(post);
+
+    } catch (error) {
         console.log(error);
-    } finally {
-        console.log("se ejecuta si o si");
     }
-
-
 }
 
-buscar();
-
-console.log("fin del codigo");
+findPostById(21);
